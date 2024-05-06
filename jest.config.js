5cc06d1fe6948780c989793ee4b23d773490fd99
Vinnys Lins <vinnysflins@gmail.com>
2024-05-06 14:22:13 -0300

    chore: setup jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  roots: ["<rootDir>/src"],
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+};
+
+module.exports = config;
