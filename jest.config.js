acd277e420c4a9a7cd0f589adcd6bbf37dff2476
Vinnys Lins <vinnysflins@gmail.com>
2024-06-25 18:59:02 -0300

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 /** @type {import('jest').Config} */
 const config = {
   collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
   transform: {
-    ".+\\.ts$": "ts-jest",
+    '.+\\.ts$': 'ts-jest',
   },
-};
+}
 
-module.exports = config;
+module.exports = config
