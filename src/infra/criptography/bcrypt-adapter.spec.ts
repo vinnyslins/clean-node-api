44cea25db7b3846e33a566b7c7fc49bd5b168baa
Vinnys Lins <vinnysflins@gmail.com>
2024-07-02 17:00:44 -0300

    feat: setup bcrypt adapter

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,31 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  hash: () => Promise.resolve('hashed_value'),
+}))
+
+const salt = 12
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  it('shoud call bcrypt with correct data', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash')
+
+    await sut.encrypt('any_value')
+
+    expect(bcrypt.hash).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('should return a hash on success', async () => {
+    const sut = makeSut()
+
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hashed_value')
+  })
+})
