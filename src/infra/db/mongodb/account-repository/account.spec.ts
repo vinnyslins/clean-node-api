10acf08754988f6263f29918df41dde6d1c7b800
Vinnys Lins <vinnysflins@gmail.com>
2024-07-03 01:18:09 -0300

    feat: create account mongo repository

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,7 @@
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+const makeSut = (): AccountMongoRepository => new AccountMongoRepository()
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -9,19 +12,24 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
-  // it('should return an account on success', () => {
-  //   const sut = new AccountMongoRepository()
-  //   const account = sut.add({
-  //     name: 'any_name',
-  //     email: 'any_email@mail.com',
-  //     password: 'any_password',
-  //   })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
 
-  //   expect(account).toEqual({
-  //     id: expect.any(String),
-  //     name: 'any_name',
-  //     email: 'any_email@mail.com',
-  //     password: 'any_password',
-  //   })
-  // })
+  it('should return an account on success', async () => {
+    const sut = makeSut()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    })
+
+    expect(account).toEqual({
+      id: expect.any(String),
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    })
+  })
 })
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,16 +1,17 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
 
   async connect(uri: string): Promise<void> {
-    this.client = await MongoClient.connect(uri, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true
-    })
+    this.client = await MongoClient.connect(uri)
   },
 
   async disconnect(): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
   },
 }
